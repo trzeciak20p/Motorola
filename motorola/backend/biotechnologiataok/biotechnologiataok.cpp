25cf00100d5d70ee14dd23294dd44e012a970535
szaszkowskap20 <91627531+szaszkowskap20@users.noreply.github.com>
2023-03-05 19:13:45 +0100

    Add files via upload

--- motorola/backend/biotechnologiataok/biotechnologiataok.cpp
+++ motorola/backend/biotechnologiataok/biotechnologiataok.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cmath>
 //zaraz ogarniemy wczytywanie z pliku sekwencji
 using namespace std;
 
@@ -27,7 +28,7 @@ bool isRNA(string sequence) {
 string changeDNAtoRNA(string sequence) {
     for (int i = 0; i < sequence.length(); i++) {
         if (sequence[i] == 'T')
-            sequence[i] = 'U';   
+            sequence[i] = 'U';
     }
     return sequence;
 }
@@ -38,37 +39,46 @@ vector < string> proteins;  //zestaw wszystkich potencjalnych białek(z odpowien
 void createCodon(string sequence) {     //tworzenie przesunięć sekwencji w taki sposób, aby rozpoczynały się od kodonu startowego
     for (int i = 0; i < sequence.length(); i++) {
         if (sequence[i] == 'A' && sequence[i + 1] == 'U' && sequence[i + 2] == 'G') {
-            
+
             codons.push_back(sequence.substr(i, sequence.length() - i));
         }
     }
 }
 
+float proteinWeight = 0;
+int h = 0;
+
 string createProteins(string sequence) {    //tworzenie białek
     string aminoSequence = "";
     char amino;
+    proteinWeight = 0;
+    h = 0;
     string protein = "";
-    //dzielimy sekwencję na stringi długości 3, następnie sprawdzamy jakiemu aminokwasowi jest string przypożądkowany, tworzymy białko 
+    //dzielimy sekwencję na stringi długości 3, następnie sprawdzamy jakiemu aminokwasowi jest string przypożądkowany, tworzymy białko
     for (int i = 0; i < sequence.length(); i += 3) {
         aminoSequence = sequence.substr(i, 3);
-        
+
 
         switch (aminoSequence[0]) {
             //pierwszy nuekleoid
-            case 'U': 
+            case 'U':
 
                 switch (aminoSequence[1]) {
                     //drugi nukleoid
-                    case 'U': 
+                    case 'U':
                         //trzeci nukleoid
                         switch (aminoSequence[2]) {
                             case 'U':
                             case 'C':
-                                amino = 'F';   
+                                amino = 'F';
+                                proteinWeight += 165.2;
+                                h += 3;
                                 break;
                             case 'A':
                             case 'G':
                                 amino = 'L';
+                                proteinWeight += 131.2;
+                                h += 3;
                                 break;
                             default:
                                 break;
@@ -77,6 +87,8 @@ string createProteins(string sequence) {    //tworzenie białek
                     case 'C':
                         //trzeci nukleoid
                         amino = 'S';
+                        proteinWeight += 105.09;
+                        h += 4;
                         break;
                     case 'A':
                         //trzeci nukleoid
@@ -84,10 +96,12 @@ string createProteins(string sequence) {    //tworzenie białek
                             case 'U':
                             case 'C':
                                 amino = 'Y';
+                                proteinWeight += 181.2;
+                                h += 4;
                                 break;
                             case 'A':
                             case 'G':
-                                amino = 'X';
+                                amino = 'X'; //koniec białka
                                 break;
                             default:
                                 break;
@@ -99,12 +113,16 @@ string createProteins(string sequence) {    //tworzenie białek
                         case 'U':
                         case 'C':
                             amino = 'C';
+                            proteinWeight += 121.2;
+                            h += 4;
                             break;
                         case 'A':
-                            amino = 'X';
+                            amino = 'X'; //koniec białka
                             break;
                         case 'G':
                             amino = 'W';
+                            proteinWeight += 204.2;
+                            h += 4;
                             break;
                         default:
                             break;
@@ -114,15 +132,19 @@ string createProteins(string sequence) {    //tworzenie białek
                         break;
                 }
                 break;
-            case 'C': 
+            case 'C':
                 switch (aminoSequence[1]) {
                     //drugi nukleoid
                     case 'U':
                         amino = 'L';
+                        proteinWeight += 131.2;
+                        h += 3;
                         break;
 
                     case 'C':
                         amino = 'P';
+                        proteinWeight += 115.1;
+                        h += 2;
                         break;
                     case 'A':
                         //trzeci nukleoid
@@ -130,17 +152,23 @@ string createProteins(string sequence) {    //tworzenie białek
                         case 'U':
                         case 'C':
                             amino = 'H';
+                            proteinWeight += 155.2;
+                            h += 4;
                             break;
                         case 'A':
                         case 'G':
                             amino = 'Q';
+                            proteinWeight += 146.1;
+                            h += 5;
                             break;
                         default:
                             break;
                         }
                         break;
                     case 'G':
-                        amino = 'L';
+                        amino = 'R';
+                        proteinWeight += 174.2;
+                        h += 8;
                         break;
                     default:
                         break;
@@ -156,9 +184,13 @@ string createProteins(string sequence) {    //tworzenie białek
                         case 'C':
                         case 'A':
                             amino = 'I';
+                            proteinWeight += 131.2;
+                            h += 3;
                             break;
                         case 'G':
                             amino = 'M';
+                            proteinWeight += 149.2;
+                            h += 3;
                             break;
                         default:
                             break;
@@ -167,6 +199,8 @@ string createProteins(string sequence) {    //tworzenie białek
                     case 'C':
                         //trzeci nukleoid
                         amino = 'T';
+                        proteinWeight += 119.1;
+                        h += 4;
                         break;
                     case 'A':
                         //trzeci nukleoid
@@ -174,10 +208,14 @@ string createProteins(string sequence) {    //tworzenie białek
                         case 'U':
                         case 'C':
                             amino = 'N';
+                            proteinWeight += 132.1;
+                            h += 5;
                             break;
                         case 'A':
                         case 'G':
                             amino = 'K';
+                            proteinWeight += 146.2;
+                            h += 6;
                             break;
                         default:
                             break;
@@ -189,10 +227,14 @@ string createProteins(string sequence) {    //tworzenie białek
                             case 'U':
                             case 'C':
                                 amino = 'S';
+                                proteinWeight += 105.09;
+                                h += 4;
                                 break;
                             case 'A':
                             case 'G':
                                 amino = 'R';
+                                proteinWeight += 174.2;
+                                h += 8;
                                 break;
                             default:
                                 break;
@@ -207,11 +249,15 @@ string createProteins(string sequence) {    //tworzenie białek
                     //drugi nukleoid
                     case 'U':
                         amino = 'V';
+                        proteinWeight += 117.1;
+                        h += 3;
                         break;
 
                     case 'C':
                         //trzeci nukleoid
                         amino = 'A';
+                        proteinWeight += 89.09;
+                        h += 3;
                         break;
                     case 'A':
                         //trzeci nukleoid
@@ -219,10 +265,14 @@ string createProteins(string sequence) {    //tworzenie białek
                         case 'U':
                         case 'C':
                             amino = 'D';
+                            proteinWeight += 133.1;
+                            h += 3;
                             break;
                         case 'A':
                         case 'G':
                             amino = 'E';
+                            proteinWeight += 147.1;
+                            h += 3;
                             break;
                         default:
                             break;
@@ -230,28 +280,38 @@ string createProteins(string sequence) {    //tworzenie białek
                         break;
                     case 'G':
                         amino = 'G';
+                        proteinWeight += 75.07;
+                        h += 3;
                         break;
-                    
+
                     default:
                         break;
-                    
+
                 }
                 break;
             default:
                 break;
-         
 
 
-            
+
+
 
         }
-        
+
         protein += amino;
 
     }
     return protein;
-   
+
 }
+
+float proteinMW(int proteinlength) {    //podajemy ilość aminokwasów w białku
+
+    float mw = proteinWeight - (18.02 * (proteinlength - 1));
+    return mw;
+
+}
+
 int main()
 {
     //sekwencja z pliku
@@ -269,7 +329,7 @@ int main()
     string potencialProtein;    //potencjalne białka, jeszcze nie zweryfikowane pod względem ostatniego kodonu
     //wczytywanie sekwencji do momentu kiedy nie będzie poprawna
     do {
-        
+
         cout << "Podaj sekwencę RNA!\n";
         cin >> sequenceRNA;
     } while (isSequenceCorrect(sequenceRNA) == true);
@@ -284,6 +344,8 @@ int main()
         potencialProtein = createProteins(codons.at(i));    //tworzenie białka
         if (potencialProtein[potencialProtein.length() - 1] == 'X') {   //sprawdzenie czy kończy się odpowiedni kodonem
             proteins.push_back(potencialProtein);   //dodadenie potencjalnego białka do wektora proteins
+            cout << "Waga bialka: " << proteinMW(potencialProtein.length() - 1) << endl;    //robimy to tylko na białkach zakończonych X na końcu, odejmujemy od długości sekwencji X ze względu na to że nie istnieje aminokwas odpowiadający tym kodonem(nie ma jego wagi), on nie jest elementem tego białka
+            cout << "PH: " << -log(h) << endl;
         }
     }
     cout << "ile bialek: " << proteins.size();
